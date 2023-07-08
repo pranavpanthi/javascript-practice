3cf605cf50f6f6cb040d90fde319111150f1b57d
pranavpanthi <panthipranav@gmail.com>
2023-07-08 14:33:15 +0545

    added Harry Potter

--- classes.js
+++ classes.js
@@ -10,7 +10,9 @@ class Book {
 let myBook = new Book ('The Meditation', 'Marcus Alerius', 250, true);
 let mysecondBook = new Book ('Decipline', 'Prahlad Panthi', 150, true);
 let mythirdBook = new Book ('Dedication', 'Pranav Panthi', 100, false);
+let myforthBook = new Book ('Harry Potter', 'JK Rolling', 450, true);
 
 console.log(myBook);
 console.log(mysecondBook);
 console.log(mythirdBook);
+console.log(myforthBook);
