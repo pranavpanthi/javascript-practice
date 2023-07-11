5934c2f0e161a6d3e93982b82ef9a03ffa6166d9
pranavpanthi <panthipranav@gmail.com>
2023-07-11 08:21:21 +0545

    .\WorkingWithArrays.js

---
+++ WorkingWithArrays.js
@@ -0,0 +1,8 @@
+let guests = ['Pranav Panthi', 'Pramod Subedi'];
+
+guests.unshift('Prahlad Panthi');
+guests.pop();
+guests.push('Pernis Subedi');
+
+console.log(guests);
+console.log(guests.length);
