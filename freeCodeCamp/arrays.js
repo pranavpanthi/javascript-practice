8652f06b91792abaad032bdf06f4404bf23cdf07
pranavpanthi <panthipranav@gmail.com>
2023-07-24 11:33:36 +0545

    .\arrays.js

---
+++ freeCodeCamp/arrays.js
@@ -0,0 +1,9 @@
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift();
+ourArray.unshift("Happy");
+
+var myArray =[["John", 23], ["dog", 3]];
+myArray.shift();
+myArray.unshift(["Paul", 35]);
+
+console.log(ourArray, myArray);
