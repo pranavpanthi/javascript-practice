3bcc2d5b4eadd042e65f62ddf84e3ef6ba6ac351
pranavpanthi <panthipranav@gmail.com>
2023-07-24 10:55:48 +0545

    .\variables.js

---
+++ freeCodeCamp/variables.js
@@ -0,0 +1,9 @@
+var firstName = "Prahlad";
+
+firstLetterOfFirstName = firstName[0];
+
+var firstLetterOfLastName = "";
+var lastName = "Panthi";
+
+firstLetterOfLastName = lastName[0];
+console.log(firstLetterOfLastName);
