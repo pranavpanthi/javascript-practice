33872e4f09abfee73d7fa82b766154513e4005de
pranavpanthi <panthipranav@gmail.com>
2023-07-25 09:16:27 +0545

    .\standInLine.js

---
+++ freeCodeCamp/ifStatements.js
@@ -0,0 +1,19 @@
+function ourTrueOrFalse(isItTrue) {
+    if (isItTrue) {
+        return "Yes, it's true";
+    }
+    return "No, it's false";
+}
+
+function TrueOrFalse(wasThatTrue) {
+    if (wasThatTrue) {
+        return "Yes, that was true";
+    }
+    return "No, that was false";
+}
+
+console.log(ourTrueOrFalse(true));
+console.log(TrueOrFalse(true));
+console.log(ourTrueOrFalse(false));
+console.log(TrueOrFalse(false));
+
